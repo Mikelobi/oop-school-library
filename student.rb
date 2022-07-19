a98a6bd53aa3f9561d98aaf45f403331d4cb8347
Michael Ugochukwu <mark.omulu@gmail.com>
2022-07-19 12:24:37 +0100

    add `classroom` to the initialize constructor

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 # student class
 class Student < Person
-  def initialize(age:, name: 'unknown', parent_permission: true)
+  def initialize(classroom:, age:, name: 'unknown', parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
