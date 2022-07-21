a505cb5049d2110326aedd7cb51ffe25705fc4ae
Michael Ugochukwu <mark.omulu@gmail.com>
2022-07-21 01:32:32 +0100

    Create `book`, `classroom` and `rental` classes

--- person.rb
+++ person.rb
@@ -1,13 +1,14 @@
-# Person class
+# Person class # rubocop:todo Layout/EndOfLine
 class Person
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age:, name: 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
