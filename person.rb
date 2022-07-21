845567683423d43bdd2cacbb193547a1016903f1
Michael Ugochukwu <mark.omulu@gmail.com>
2022-07-21 02:06:05 +0100

    Create `book`, and `rental` classes | add relationship between rental, book and person

--- person.rb
+++ person.rb
@@ -1,17 +1,30 @@
+require_relative './nameable'
+require_relative './decorator'
+require_relative './rental'
 # Person class
-class Person
+class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age:, name: 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    name
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
   end
 
   private
