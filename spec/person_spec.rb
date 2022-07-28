a7c9c31611a7593d2348d2138d4802c1c0bf722b
christian <christianbiringanine22@gmail.com>
2022-07-28 12:40:51 +0200

    Add the person testing

---
+++ spec/person_spec.rb
@@ -0,0 +1,20 @@
+require './person'
+
+describe Person do
+  context 'it must instantiate an object' do
+    it 'must be true' do
+      age = 45
+      name = 'MJ'
+      id = 100
+      parent_permission = true
+      rental = '2020/07/20'
+
+      person = Person.new(age: age, name: name, id: id, parent_permission: parent_permission)
+      expect(person).to be_instance_of Person
+
+      expect(person.can_use_services?).to be true
+      expect(person.add_rental(rental)).to eq(['2020/07/20'])
+      expect(person.of_age?).to be true
+    end
+  end
+end
