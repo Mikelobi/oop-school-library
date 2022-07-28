49ce2c6a572bcc99685d499992907401eb7ce1e3
christian <christianbiringanine22@gmail.com>
2022-07-28 14:46:13 +0200

    Fix linters errors

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -2,22 +2,22 @@ require './class_room'
 require './student'
 
 describe ClassRoom do
-    context 'testing classroom class' do
-        classroom = ClassRoom.new(label: 'Physics')
-        student = Student.new(age: 11, name: 'Bright', parent_permission: true, id: 1)
-        classroom.add_students(student)
+  context 'testing classroom class' do
+    classroom = ClassRoom.new(label: 'Physics')
+    student = Student.new(age: 11, name: 'Bright', parent_permission: true, id: 1)
+    classroom.add_students(student)
 
-        it 'should name a label' do
-            expect(classroom.label).to eq(label: 'Physics')
-        end
-
-        it 'should have atleast one student' do
-            expect(classroom.students.length).to eq(1)
-        end
-
-        it 'should have a student' do
-            student_obj = classroom.students.first
-            expect(student_obj.name).to eq(student.name)
-        end
+    it 'should name a label' do
+      expect(classroom.label).to eq(label: 'Physics')
     end
+
+    it 'should have atleast one student' do
+      expect(classroom.students.length).to eq(1)
+    end
+
+    it 'should have a student' do
+      student_obj = classroom.students.first
+      expect(student_obj.name).to eq(student.name)
+    end
+  end
 end
--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -3,17 +3,17 @@ require './person'
 require './book'
 
 describe Rental do
-    context 'testing the Rental class' do
-        book = Book.new(title: 'Great Wall', author: 'luther', id: 3)
-        person = Person.new(name: 'Collins', age: 11, id: 2, parent_permission: true)
-        rental = Rental.new('11/10/2020', book, person)
+  context 'testing the Rental class' do
+    book = Book.new(title: 'Great Wall', author: 'luther', id: 3)
+    person = Person.new(name: 'Collins', age: 11, id: 2, parent_permission: true)
+    rental = Rental.new('11/10/2020', book, person)
 
-        it 'should have a title' do
-            expect(rental.book.title).to eq('Great Wall')
-        end
-
-        it 'person name should be Collins' do
-            expect(rental.person.name).to eq('Collins')
-        end
+    it 'should have a title' do
+      expect(rental.book.title).to eq('Great Wall')
     end
+
+    it 'person name should be Collins' do
+      expect(rental.person.name).to eq('Collins')
+    end
+  end
 end
