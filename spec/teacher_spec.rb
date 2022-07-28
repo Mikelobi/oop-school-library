3b9d0d894df22630fa43ca78544210b6cbc2cafc
Michael Ugochukwu <mark.omulu@gmail.com>
2022-07-28 12:51:22 +0100

    Test for `teacher` class

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,15 @@
+require './teacher'
+
+describe Teacher do
+    context 'testing Teacher class' do
+        teacher = Teacher.new(name: 'David', age: 22, id: 1, parent_permission: true, specialization: 'Physics')
+
+        it 'should have a name' do
+            expect(teacher.name).to eq('David')
+        end
+
+        it 'should specialize in Physics' do
+            expect(teacher.specialization).to eq('Physics')
+        end
+    end
+end
